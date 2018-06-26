2399d27b005385527852b510c77ab17c55a99b0a
RCXcrafter <rcxcrafter@gmail.com>
2018-06-26 15:33:42 +0200

    give pick and shovel proper attack stats

--- src/main/java/com/axanthic/blab/items/ToolPickaxe.java
+++ src/main/java/com/axanthic/blab/items/ToolPickaxe.java
@@ -3,15 +3,23 @@ package com.axanthic.blab.items;
 import com.axanthic.blab.Blab;
 import com.axanthic.blab.ModInformation;
 import com.axanthic.blab.Resources.CompleteToolMaterial;
+import com.google.common.collect.HashMultimap;
+import com.google.common.collect.Multimap;
 
+import net.minecraft.entity.SharedMonsterAttributes;
+import net.minecraft.entity.ai.attributes.AttributeModifier;
+import net.minecraft.inventory.EntityEquipmentSlot;
 import net.minecraft.item.ItemPickaxe;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.text.translation.I18n;
 
 public class ToolPickaxe extends ItemPickaxe {
 
+	public CompleteToolMaterial material;
+
 	public ToolPickaxe(CompleteToolMaterial material) {
 		super(material.material);
+		this.material = material;
 		this.setCreativeTab(Blab.modTab);
 		this.setUnlocalizedName("generic.pickaxe");
 		this.setRegistryName(ModInformation.ID, "pickaxe_" + material.material.name().substring(ModInformation.ID.length() + 1));
@@ -24,5 +32,16 @@ public class ToolPickaxe extends ItemPickaxe {
 		} catch (Exception e) {
 			return String.format(I18n.translateToLocal(this.getUnlocalizedName() + ".name"), "");
 		}
+	}
+
+	@Override
+	public Multimap<String, AttributeModifier> getItemAttributeModifiers(EntityEquipmentSlot equipmentSlot) {
+		Multimap<String, AttributeModifier> multimap = HashMultimap.<String, AttributeModifier>create();
+
+		if (equipmentSlot == EntityEquipmentSlot.MAINHAND) {
+			multimap.put(SharedMonsterAttributes.ATTACK_DAMAGE.getName(), new AttributeModifier(ATTACK_DAMAGE_MODIFIER, "Weapon modifier", (double)this.material.material.getAttackDamage() + 0.5D, 0));
+			multimap.put(SharedMonsterAttributes.ATTACK_SPEED.getName(), new AttributeModifier(ATTACK_SPEED_MODIFIER, "Weapon modifier", (double)this.material.attackSpeed - 1.7D, 0));
+		}
+		return multimap;
 	}
 }
--- src/main/java/com/axanthic/blab/items/ToolShovel.java
+++ src/main/java/com/axanthic/blab/items/ToolShovel.java
@@ -3,15 +3,23 @@ package com.axanthic.blab.items;
 import com.axanthic.blab.Blab;
 import com.axanthic.blab.ModInformation;
 import com.axanthic.blab.Resources.CompleteToolMaterial;
+import com.google.common.collect.HashMultimap;
+import com.google.common.collect.Multimap;
 
+import net.minecraft.entity.SharedMonsterAttributes;
+import net.minecraft.entity.ai.attributes.AttributeModifier;
+import net.minecraft.inventory.EntityEquipmentSlot;
 import net.minecraft.item.ItemSpade;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.text.translation.I18n;
 
 public class ToolShovel extends ItemSpade {
 
+	public CompleteToolMaterial material;
+
 	public ToolShovel(CompleteToolMaterial material) {
 		super(material.material);
+		this.material = material;
 		this.setCreativeTab(Blab.modTab);
 		this.setUnlocalizedName("generic.shovel");
 		this.setRegistryName(ModInformation.ID, "shovel_" + material.material.name().substring(ModInformation.ID.length() + 1));
@@ -24,5 +32,16 @@ public class ToolShovel extends ItemSpade {
 		} catch (Exception e) {
 			return String.format(I18n.translateToLocal(this.getUnlocalizedName() + ".name"), "");
 		}
+	}
+
+	@Override
+	public Multimap<String, AttributeModifier> getItemAttributeModifiers(EntityEquipmentSlot equipmentSlot) {
+		Multimap<String, AttributeModifier> multimap = HashMultimap.<String, AttributeModifier>create();
+
+		if (equipmentSlot == EntityEquipmentSlot.MAINHAND) {
+			multimap.put(SharedMonsterAttributes.ATTACK_DAMAGE.getName(), new AttributeModifier(ATTACK_DAMAGE_MODIFIER, "Weapon modifier", (double)this.material.material.getAttackDamage() + 1.0D, 0));
+			multimap.put(SharedMonsterAttributes.ATTACK_SPEED.getName(), new AttributeModifier(ATTACK_SPEED_MODIFIER, "Weapon modifier", (double)this.material.attackSpeed - 1.9D, 0));
+		}
+		return multimap;
 	}
 }
