1c0b0c30d2ad12241fa870c3ca69aba43198d0b3
ChowChowSonic <joeyant1367@gmail.com>
2019-05-27 13:00:07 -0400

    Update ModInformation.java

--- src/main/java/com/axanthic/blab/ModInformation.java
+++ src/main/java/com/axanthic/blab/ModInformation.java
@@ -5,7 +5,7 @@ public class ModInformation {
 	public static final String NAME = "Blab";
 	public static final String ID = "blab";
 	public static final String DEPEND =  "";
-	public static final String VERSION = "0.0.0";
+	public static final String VERSION = "I lost count Lmao";
 	public static final String CLIENTPROXY = "com.axanthic.blab.proxy.ClientProxy";
 	public static final String COMMONPROXY = "com.axanthic.blab.proxy.CommonProxy";
 	public static final String NETWORK_CHANNEL_NAME = "blab";
