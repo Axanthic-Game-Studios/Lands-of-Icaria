3fc1d83facb908c4189469501ad33efd1afde512
RCXcrafter <rcxcrafter@gmail.com>
2019-06-05 16:28:52 +0200

    add shape for laurel tree

--- src/main/java/com/axanthic/loi/worldgen/feature/WorldGenLOITree.java
+++ src/main/java/com/axanthic/loi/worldgen/feature/WorldGenLOITree.java
@@ -1,12 +1,9 @@
 package com.axanthic.loi.worldgen.feature;
 
-import java.util.Random;
-
 import net.minecraft.block.BlockBush;
 import net.minecraft.block.BlockRotatedPillar;
 import net.minecraft.block.material.Material;
 import net.minecraft.block.state.IBlockState;
-import net.minecraft.init.Blocks;
 import net.minecraft.util.EnumFacing;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
@@ -22,81 +19,6 @@ public abstract class WorldGenLOITree extends WorldGenAbstractTree {
 	}
 
 	public abstract WorldGenLOITree init();
-
-	@Override
-	public boolean generate(World worldIn, Random rand, BlockPos position) {
-		int i = rand.nextInt(3) + 5;
-
-		boolean flag = true;
-
-		if (position.getY() >= 1 && position.getY() + i + 1 <= 256) {
-			for (int j = position.getY(); j <= position.getY() + 1 + i; ++j) {
-				int k = 1;
-
-				if (j == position.getY()) {
-					k = 0;
-				}
-
-				if (j >= position.getY() + 1 + i - 2) {
-					k = 2;
-				}
-
-				BlockPos.MutableBlockPos blockpos$mutableblockpos = new BlockPos.MutableBlockPos();
-
-				for (int l = position.getX() - k; l <= position.getX() + k && flag; ++l) {
-					for (int i1 = position.getZ() - k; i1 <= position.getZ() + k && flag; ++i1) {
-						if (j >= 0 && j < worldIn.getHeight()) {
-							if (!this.canGrowThrough(worldIn, blockpos$mutableblockpos.setPos(l, j, i1))) {
-								flag = false;
-							}
-						} else {
-							flag = false;
-						}
-					}
-				}
-			}
-			if (!flag) {
-				return false;
-			} else {
-				BlockPos down = position.down();
-				IBlockState state = worldIn.getBlockState(down);
-				boolean isSoil = state.getBlock().canSustainPlant(state, worldIn, down, net.minecraft.util.EnumFacing.UP, (net.minecraft.block.BlockSapling)Blocks.SAPLING);
-
-				if (isSoil && position.getY() < worldIn.getHeight() - i - 1) {
-					state.getBlock().onPlantGrow(state, worldIn, down, position);
-
-					for (int i2 = position.getY() - 3 + i; i2 <= position.getY() + i; ++i2) {
-						int k2 = i2 - (position.getY() + i);
-						int l2 = 1 - k2 / 2;
-
-						for (int i3 = position.getX() - l2; i3 <= position.getX() + l2; ++i3) {
-							int j1 = i3 - position.getX();
-
-							for (int k1 = position.getZ() - l2; k1 <= position.getZ() + l2; ++k1) {
-								int l1 = k1 - position.getZ();
-
-								if (Math.abs(j1) != l2 || Math.abs(l1) != l2 || rand.nextInt(2) != 0 && k2 != 0) {
-									BlockPos blockpos = new BlockPos(i3, i2, k1);
-									this.placeLeafAt(worldIn, blockpos);
-								}
-							}
-						}
-					}
-					for (int j2 = 0; j2 < i; ++j2) {
-						BlockPos upN = position.up(j2);
-						IBlockState state2 = worldIn.getBlockState(upN);
-
-						this.placeLogAt(worldIn, upN);
-					}
-					return true;
-				} else {
-					return false;
-				}
-			}
-		} else {
-			return false;
-		}
-	}
 
 	@Override
 	public boolean isReplaceable(World world, BlockPos pos) {
