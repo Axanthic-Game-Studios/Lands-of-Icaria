c956dc37a3d050413ea09a758038f399c19c52f5
RCXcrafter <rcxcrafter@gmail.com>
2019-09-27 23:40:10 +0200

    refine ore generation

--- src/main/java/com/axanthic/loi/LOIConfig.java
+++ src/main/java/com/axanthic/loi/LOIConfig.java
@@ -21,4 +21,91 @@ public class LOIConfig {
 		public Boolean albedo = true;
 
 	}
+
+	@Name("worldgeneration")
+	@LangKey("loi.config.worldgen")
+	@Comment("World Generation settings")
+	public static CategoryWorldgen world = new CategoryWorldgen();
+
+	public static class CategoryWorldgen {
+
+		@RequiresMcRestart
+		@LangKey("loi.config.worldgen.lignite")
+		public OreSettings lignite = new OreSettings(-0.02F, -0.0F, 1.0F, 0000);
+
+		@RequiresMcRestart
+		@LangKey("loi.config.worldgen.chalkos")
+		public OreSettings chalkos = new OreSettings(-0.115F, -0.1F, 0.75F, 2000);
+
+		@RequiresMcRestart
+		@LangKey("loi.config.worldgen.dolomite")
+		public OreSettings dolomite = new OreSettings(0.0F, 0.02F, 1.0F, 4000);
+
+		@RequiresMcRestart
+		@LangKey("loi.config.worldgen.kassiteros")
+		public OreSettings kassiteros = new OreSettings(0.1F, 0.115F, 0.75F, 6000);
+
+		@RequiresMcRestart
+		@LangKey("loi.config.worldgen.molibos")
+		public OreSettings molibos = new OreSettings(0.2F, 0.21F, 0.5F, 8000);
+
+		@RequiresMcRestart
+		@LangKey("loi.config.worldgen.sliver")
+		public OreSettings sliver = new OreSettings(-0.22F, -0.2F, 1.0F, 1000);
+
+		@RequiresMcRestart
+		@LangKey("loi.config.worldgen.vanadium")
+		public OreSettings vanadium = new OreSettings(-0.31F, -0.3F, 0.5F, 3000);
+
+		@RequiresMcRestart
+		@LangKey("loi.config.worldgen.anthracite")
+		public OreSettings anthracite = new OreSettings(0.3F, 0.32F, 1.0F, 5000);
+
+		@RequiresMcRestart
+		@LangKey("loi.config.worldgen.sideros")
+		public OreSettings sideros = new OreSettings(0.4F, 0.415F, 0.75F, 7000);
+
+		@RequiresMcRestart
+		@LangKey("loi.config.worldgen.molybdenum")
+		public OreSettings molybdenum = new OreSettings(0.5F, 0.51F, 0.5F, 9000);
+
+		@RequiresMcRestart
+		@LangKey("loi.config.worldgen.hyliastrum")
+		public OreSettings hyliastrum = new OreSettings(-0.62F, -0.6F, 1.0F, 0000);
+
+		@RequiresMcRestart
+		@LangKey("loi.config.worldgen.abyssalessence")
+		public OreSettings abyssalEssence = new OreSettings(-0.615F, -0.6F, 0.75F, 2000);
+
+		@RequiresMcRestart
+		@LangKey("loi.config.worldgen.bluridium")
+		public OreSettings bluridium = new OreSettings(-0.71F, -0.7F, 0.5F, 4000);
+
+		public static class OreSettings {
+
+			@RequiresMcRestart
+			@Name("veinbegin")
+			@LangKey("loi.config.worldgen.ore.indexbegin")
+			public Float indexBegin;
+			@RequiresMcRestart
+			@Name("veinend")
+			@LangKey("loi.config.worldgen.ore.indexend")
+			public Float indexEnd;
+			@RequiresMcRestart
+			@Name("veinscale")
+			@LangKey("loi.config.worldgen.ore.noisesize")
+			public Float noiseSize;
+			@RequiresMcRestart
+			@Name("generationoffset")
+			@LangKey("loi.config.worldgen.ore.offset")
+			public int offset;
+
+			public OreSettings (Float indexBegin, Float indexEnd, Float noiseSize, int offset) {
+				this.indexBegin = indexBegin;
+				this.indexEnd = indexEnd;
+				this.noiseSize = noiseSize;
+				this.offset = offset;
+			}
+		}
+	}
 }
--- src/main/java/com/axanthic/loi/worldgen/dimension/OreGeneratorLOI.java
+++ src/main/java/com/axanthic/loi/worldgen/dimension/OreGeneratorLOI.java
@@ -3,24 +3,28 @@ package com.axanthic.loi.worldgen.dimension;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.init.Blocks;
 import net.minecraft.world.chunk.ChunkPrimer;
+
+import com.axanthic.loi.LOIConfig;
 import com.axanthic.loi.Resources;
+import com.axanthic.loi.LOIConfig.CategoryWorldgen.OreSettings;
 import com.axanthic.loi.utils.PerlinNoise;
 import com.google.common.base.Predicate;
 
 public class OreGeneratorLOI {
-	private static final LOIOregen ligniteGenerator = new LOIOregen(Resources.ore.getBlock().getStateFromMeta(0), new RockPredicate(0), -0.1F, 0.1F, 0.5F);
-	private static final LOIOregen chalkosGenerator = new LOIOregen(Resources.ore.getBlock().getStateFromMeta(1), new RockPredicate(0), -0.2F, 0.2F, 1.0F);
-	private static final LOIOregen kassiterosGenerator = new LOIOregen(Resources.ore.getBlock().getStateFromMeta(2), new RockPredicate(1), -0.2F, 0.2F, 0.5F);
-	private static final LOIOregen dolomiteGenerator = new LOIOregen(Resources.ore.getBlock().getStateFromMeta(3), new RockPredicate(1), -0.2F, 0.2F, 0.75F);
-	private static final LOIOregen molibosGenerator = new LOIOregen(Resources.ore.getBlock().getStateFromMeta(4), new RockPredicate(1), -0.2F, 0.2F, 1.0F);
-	private static final LOIOregen vanadiumGenerator = new LOIOregen(Resources.ore.getBlock().getStateFromMeta(5), new RockPredicate(2), -0.2F, 0.2F, 0.5F);
-	private static final LOIOregen sliverGenerator = new LOIOregen(Resources.ore.getBlock().getStateFromMeta(6), new RockPredicate(2), -0.2F, 0.2F, 1.0F);
-	private static final LOIOregen siderosGenerator = new LOIOregen(Resources.ore.getBlock().getStateFromMeta(7), new RockPredicate(3), -0.2F, 0.2F, 0.5F);
-	private static final LOIOregen anthraciteGenerator = new LOIOregen(Resources.ore.getBlock().getStateFromMeta(8), new RockPredicate(3), -0.2F, 0.2F, 0.75F);
-	private static final LOIOregen molybdenumGenerator = new LOIOregen(Resources.ore.getBlock().getStateFromMeta(9), new RockPredicate(3), -0.2F, 0.2F, 1.0F);
-	private static final LOIOregen bluridiumGenerator = new LOIOregen(Resources.ore.getBlock().getStateFromMeta(10), new RockPredicate(4), -0.2F, 0.2F, 0.5F);
-	private static final LOIOregen hyliastrumGenerator = new LOIOregen(Resources.ore.getBlock().getStateFromMeta(11), new RockPredicate(4), -0.2F, 0.2F, 0.75F);
-	private static final LOIOregen abyssalEssenceGenerator = new LOIOregen(Resources.ore.getBlock().getStateFromMeta(12), new RockPredicate(4), -0.2F, 0.2F, 1.0F);
+
+	private static final LOIOregen ligniteGenerator = new LOIOregen(Resources.ore.getBlock().getStateFromMeta(0), new RockPredicate(0), LOIConfig.world.lignite);
+	private static final LOIOregen chalkosGenerator = new LOIOregen(Resources.ore.getBlock().getStateFromMeta(1), new RockPredicate(0), LOIConfig.world.chalkos);
+	private static final LOIOregen dolomiteGenerator = new LOIOregen(Resources.ore.getBlock().getStateFromMeta(3), new RockPredicate(1), LOIConfig.world.dolomite);
+	private static final LOIOregen kassiterosGenerator = new LOIOregen(Resources.ore.getBlock().getStateFromMeta(2), new RockPredicate(1), LOIConfig.world.kassiteros);
+	private static final LOIOregen molibosGenerator = new LOIOregen(Resources.ore.getBlock().getStateFromMeta(4), new RockPredicate(1), LOIConfig.world.molibos);
+	private static final LOIOregen sliverGenerator = new LOIOregen(Resources.ore.getBlock().getStateFromMeta(6), new RockPredicate(2), LOIConfig.world.sliver);
+	private static final LOIOregen vanadiumGenerator = new LOIOregen(Resources.ore.getBlock().getStateFromMeta(5), new RockPredicate(2), LOIConfig.world.vanadium);
+	private static final LOIOregen anthraciteGenerator = new LOIOregen(Resources.ore.getBlock().getStateFromMeta(8), new RockPredicate(3), LOIConfig.world.anthracite);
+	private static final LOIOregen siderosGenerator = new LOIOregen(Resources.ore.getBlock().getStateFromMeta(7), new RockPredicate(3), LOIConfig.world.sideros);
+	private static final LOIOregen molybdenumGenerator = new LOIOregen(Resources.ore.getBlock().getStateFromMeta(9), new RockPredicate(3), LOIConfig.world.molybdenum);
+	private static final LOIOregen hyliastrumGenerator = new LOIOregen(Resources.ore.getBlock().getStateFromMeta(11), new RockPredicate(4), LOIConfig.world.hyliastrum);
+	private static final LOIOregen abyssalEssenceGenerator = new LOIOregen(Resources.ore.getBlock().getStateFromMeta(12), new RockPredicate(4), LOIConfig.world.abyssalEssence);
+	private static final LOIOregen bluridiumGenerator = new LOIOregen(Resources.ore.getBlock().getStateFromMeta(10), new RockPredicate(4), LOIConfig.world.bluridium);
 
 	private final PerlinNoise oreNoise;
 
@@ -29,30 +33,31 @@ public class OreGeneratorLOI {
 	}
 
 	public void generate(final int x, final int z, final int wx, final int wz, final ChunkPrimer primer) {
-		generateOre(ligniteGenerator, x, z, wx, wz, primer);
 		generateOre(chalkosGenerator, x, z, wx, wz, primer);
+		generateOre(ligniteGenerator, x, z, wx, wz, primer);
+		generateOre(molibosGenerator, x, z, wx, wz, primer);
 		generateOre(kassiterosGenerator, x, z, wx, wz, primer);
 		generateOre(dolomiteGenerator, x, z, wx, wz, primer);
-		generateOre(molibosGenerator, x, z, wx, wz, primer);
 		generateOre(vanadiumGenerator, x, z, wx, wz, primer);
 		generateOre(sliverGenerator, x, z, wx, wz, primer);
-		generateOre(anthraciteGenerator, x, z, wx, wz, primer);
-		generateOre(siderosGenerator, x, z, wx, wz, primer);
 		generateOre(molybdenumGenerator, x, z, wx, wz, primer);
+		generateOre(siderosGenerator, x, z, wx, wz, primer);
+		generateOre(anthraciteGenerator, x, z, wx, wz, primer);
 		generateOre(bluridiumGenerator, x, z, wx, wz, primer);
-		generateOre(hyliastrumGenerator, x, z, wx, wz, primer);
 		generateOre(abyssalEssenceGenerator, x, z, wx, wz, primer);
+		generateOre(hyliastrumGenerator, x, z, wx, wz, primer);
 	}
 
 	private void generateOre(LOIOregen oreGen, final int x, final int z, final int wx, final int wz, final ChunkPrimer primer) {
-		double value = this.oreNoise.noise((wx + 2221D) / (100D * oreGen.noiseSize), (wz + 2221D) / (100D * oreGen.noiseSize));
-		System.out.println("value is: " + value);
-		if (value > oreGen.indexBegin && value < oreGen.indexEnd) {
-			for(int y = ChunkGeneratorLOI.CHUNK_HEIGHT; y > 1; y--) {
-				IBlockState b = primer.getBlockState(x, y, z);
-				final IBlockState upper = primer.getBlockState(x, y + 1, z);
-				if(b.getBlock() != Blocks.AIR && upper.getBlock() == Blocks.AIR) {
-					for(int gy = 1; gy < y; gy++) {
+		float offset = this.oreNoise.noise2((wx + oreGen.offset) / (70F * oreGen.noiseSize), (wz + oreGen.offset) / (70F * oreGen.noiseSize)) * 0.25F;
+		float thinnness = Math.max(0.0F, this.oreNoise.noise2((wx + oreGen.offset) / (40F * oreGen.noiseSize), (wz + oreGen.offset) / (40F * oreGen.noiseSize)) * (oreGen.indexBegin - oreGen.indexEnd) * 1.5F);
+		for(int y = ChunkGeneratorLOI.CHUNK_HEIGHT; y > 1; y--) {
+			IBlockState b = primer.getBlockState(x, y, z);
+			final IBlockState upper = primer.getBlockState(x, y + 1, z);
+			if(b.getBlock() != Blocks.AIR && upper.getBlock() == Blocks.AIR) {
+				for(int gy = 1; gy < y; gy++) {
+					float value = this.oreNoise.noise3((wx + oreGen.offset) / (400F * oreGen.noiseSize), (y - gy) / 500F, (wz + oreGen.offset) / (400F * oreGen.noiseSize)) + offset;
+					if (value > oreGen.indexBegin + thinnness && value < oreGen.indexEnd - thinnness) {
 						b = primer.getBlockState(x, y - gy, z);
 						if(b.getBlock() == Blocks.AIR) {
 							break;
@@ -89,13 +94,19 @@ public class OreGeneratorLOI {
 		public Float indexBegin;
 		public Float indexEnd;
 		public Float noiseSize;
+		public int offset;
 
-		public LOIOregen (IBlockState ore, RockPredicate rock, Float indexBegin, Float indexEnd, Float noiseSize) {
+		public LOIOregen (IBlockState ore, RockPredicate rock, OreSettings settings) {
+			this(ore, rock, settings.indexBegin, settings.indexEnd, settings.noiseSize, settings.offset);
+		}
+
+		public LOIOregen (IBlockState ore, RockPredicate rock, Float indexBegin, Float indexEnd, Float noiseSize, int offset) {
 			this.ore = ore;
 			this.rock = rock;
 			this.indexBegin = indexBegin;
 			this.indexEnd = indexEnd;
 			this.noiseSize = noiseSize;
+			this.offset = offset;
 		}
 	}
 }
