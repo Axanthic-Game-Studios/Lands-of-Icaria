b03d7f6a91e7625dddef37653162c632e4e7511e
RCXcrafter <rcxcrafter@gmail.com>
2020-07-04 15:35:17 +0200

    make spells bright in darkness

--- src/main/java/com/axanthic/loi/render/RenderSpellBubble.java
+++ src/main/java/com/axanthic/loi/render/RenderSpellBubble.java
@@ -8,6 +8,7 @@ import com.axanthic.loi.entity.EntitySpellBubble;
 
 import net.minecraft.client.renderer.BufferBuilder;
 import net.minecraft.client.renderer.GlStateManager;
+import net.minecraft.client.renderer.OpenGlHelper;
 import net.minecraft.client.renderer.Tessellator;
 import net.minecraft.client.renderer.entity.Render;
 import net.minecraft.client.renderer.entity.RenderManager;
@@ -42,6 +43,11 @@ public class RenderSpellBubble extends Render<EntitySpellBubble> {
 			GlStateManager.enableColorMaterial();
 			GlStateManager.enableOutlineMode(this.getTeamColor(entity));
 		}
+
+		int u = 15728864;
+		int j = u % 65536;
+		int k = u / 65536;
+		OpenGlHelper.setLightmapTextureCoords(OpenGlHelper.lightmapTexUnit, (float)j, (float)k);
 
 		GlStateManager.disableCull();
 		GlStateManager.shadeModel(7425);
