a06a8493de859daf41fd1525f2c615bc5db9ece8
Xenoform55 <arrowstorm606@gmail.com>
2020-10-12 02:10:06 -0400

    Screaming Siren Prt. 1

--- src/main/java/com/axanthic/loi/entity/EntitySiren.java
+++ src/main/java/com/axanthic/loi/entity/EntitySiren.java
@@ -1,35 +1,34 @@
 package com.axanthic.loi.entity;
 
-import javax.annotation.Nullable;
-
-import com.axanthic.loi.Resources;
-
 import net.minecraft.block.Block;
+import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.EnumCreatureAttribute;
-import net.minecraft.entity.IEntityLivingData;
 import net.minecraft.entity.SharedMonsterAttributes;
 import net.minecraft.entity.ai.*;
-import net.minecraft.entity.ai.attributes.AttributeModifier;
 import net.minecraft.entity.monster.EntityCreeper;
 import net.minecraft.entity.monster.EntityIronGolem;
 import net.minecraft.entity.monster.EntityMob;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.init.SoundEvents;
-import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.network.datasync.DataParameter;
 import net.minecraft.network.datasync.DataSerializers;
 import net.minecraft.network.datasync.EntityDataManager;
 import net.minecraft.util.DamageSource;
-import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.SoundEvent;
 import net.minecraft.util.math.BlockPos;
-import net.minecraft.world.DifficultyInstance;
 import net.minecraft.world.World;
+
+import java.util.List;
 
 public class EntitySiren extends EntityMob {
 
 	private static final DataParameter<Integer> STATE = EntityDataManager.<Integer>createKey(EntityCreeper.class, DataSerializers.VARINT);
+
+	private int lastActiveTime;
+	private int timeSinceIgnited;
+	private int fuseTime = 20;
 
 	public EntitySiren(World worldIn) {
 		super(worldIn);
@@ -38,6 +37,9 @@ public class EntitySiren extends EntityMob {
 
 	protected void initEntityAI() {
 		this.tasks.addTask(0, new EntityAISwimming(this));
+		this.tasks.addTask(1, new EntityAIScream(this));
+		this.tasks.addTask(2, new EntityAILeapAtTarget(this, 0.4F));
+		this.tasks.addTask(3, new EntityAIAttackMelee(this, 1.0D, false));
 		this.tasks.addTask(5, new EntityAIMoveTowardsRestriction(this, 1.0D));
 		this.tasks.addTask(7, new EntityAIWanderAvoidWater(this, 1.0D));
 		this.tasks.addTask(8, new EntityAIWatchClosest(this, EntityPlayer.class, 8.0F));
@@ -51,6 +53,11 @@ public class EntitySiren extends EntityMob {
 		this.getEntityAttribute(SharedMonsterAttributes.FOLLOW_RANGE).setBaseValue(35.0D);
 		this.getEntityAttribute(SharedMonsterAttributes.MOVEMENT_SPEED).setBaseValue(0.2D);
 		this.getEntityAttribute(SharedMonsterAttributes.ATTACK_DAMAGE).setBaseValue(3.0D);
+	}
+
+	protected void entityInit() {
+		super.entityInit();
+		this.dataManager.register(STATE, -1);
 	}
 
 	protected SoundEvent getAmbientSound() {
@@ -77,29 +84,118 @@ public class EntitySiren extends EntityMob {
 		return EnumCreatureAttribute.UNDEAD;
 	}
 
-	@Nullable
-	protected ResourceLocation getLootTable() {
-		return Resources.LOOT_REVENANT;
-	}
-
 	public float getEyeHeight() {
 		return 1.8F;
 	}
 
-	@Nullable
-	public IEntityLivingData onInitialSpawn(DifficultyInstance difficulty, @Nullable IEntityLivingData livingdata) {
-		livingdata = super.onInitialSpawn(difficulty, livingdata);
-		this.getEntityAttribute(SharedMonsterAttributes.KNOCKBACK_RESISTANCE).applyModifier(new AttributeModifier("Random spawn bonus", this.rand.nextDouble() * 0.05000000074505806D, 0));
-		return livingdata;
+	public void writeEntityToNBT(NBTTagCompound compound)
+	{
+		super.writeEntityToNBT(compound);
+		compound.setShort("Fuse", (short)this.fuseTime);
 	}
 
-	public void onDeath(DamageSource cause) {
-		super.onDeath(cause);
-		if (cause.getTrueSource() instanceof EntityCreeper) {
-			EntityCreeper entitycreeper = (EntityCreeper)cause.getTrueSource();
-			if (entitycreeper.getPowered() && entitycreeper.ableToCauseSkullDrop()) {
-				entitycreeper.incrementDroppedSkulls();
-				this.entityDropItem(new ItemStack(Resources.mobHeadRevenant, 1, 0), 0.0F);
+	public void readEntityFromNBT(NBTTagCompound compound)
+	{
+		super.readEntityFromNBT(compound);
+		if (compound.hasKey("Fuse", 99))
+		{
+			this.fuseTime = compound.getShort("Fuse");
+		}
+	}
+
+	public int getCreeperState()
+	{
+		return this.dataManager.get(STATE);
+	}
+
+	public void setCreeperState(int state)
+	{
+		this.dataManager.set(STATE, state);
+	}
+
+	public void onUpdate()
+	{
+		if (this.isEntityAlive())
+		{
+			this.lastActiveTime = this.timeSinceIgnited;
+
+			int i = this.getCreeperState();
+			this.timeSinceIgnited += i;
+
+			if (this.timeSinceIgnited < 0)
+			{
+				this.timeSinceIgnited = 0;
+			}
+
+			if (this.timeSinceIgnited >= this.fuseTime)
+			{
+				this.timeSinceIgnited = this.fuseTime;
+				this.scream();
+			}
+		}
+		super.onUpdate();
+	}
+
+	private void scream()
+	{
+		if (!this.world.isRemote)
+		{
+			List list = world.getEntitiesWithinAABB(EntityPlayer.class, this.getEntityBoundingBox().expand(10.0D, 10.0D, 10.0D));
+				for (int i = 0; i < list.size(); i++) {
+				Object o = list.get(i);
+					if (o instanceof EntityPlayer) {
+						((Entity) o).attackEntityFrom(DamageSource.WITHER, 4.0F);
+						this.playSound(SoundEvents.ENTITY_ENDERMEN_SCREAM, 2.0F, 0.4F);
+				}
+			}
+		}
+	}
+
+	static class EntityAIScream extends EntityAIBase
+	{
+		EntitySiren screamer;
+		EntityLivingBase target;
+
+		public EntityAIScream(EntitySiren entitycreeperIn)
+		{
+			this.screamer = entitycreeperIn;
+			this.setMutexBits(1);
+		}
+
+		public boolean shouldExecute()
+		{
+			EntityLivingBase entitylivingbase = this.screamer.getAttackTarget();
+			return this.screamer.getCreeperState() > 0 || entitylivingbase != null && this.screamer.getDistanceSq(entitylivingbase) < 9.0D;
+		}
+
+		public void startExecuting()
+		{
+			this.screamer.getNavigator().clearPath();
+			this.target = this.screamer.getAttackTarget();
+		}
+
+		public void resetTask()
+		{
+			this.target = null;
+		}
+
+		public void updateTask()
+		{
+			if (this.target == null)
+			{
+				this.screamer.setCreeperState(-1);
+			}
+			else if (this.screamer.getDistanceSq(this.target) > 49.0D)
+			{
+				this.screamer.setCreeperState(-1);
+			}
+			else if (!this.screamer.getEntitySenses().canSee(this.target))
+			{
+				this.screamer.setCreeperState(-1);
+			}
+			else
+			{
+				this.screamer.setCreeperState(1);
 			}
 		}
 	}
