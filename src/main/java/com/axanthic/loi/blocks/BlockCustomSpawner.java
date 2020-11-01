7fd804badf14726f47afc87b08b1b1d50c67ef11
RCXcrafter <rcxcrafter@gmail.com>
2020-11-01 12:29:07 +0100

    allow spawner modification with eggs

--- src/main/java/com/axanthic/loi/blocks/BlockCustomSpawner.java
+++ src/main/java/com/axanthic/loi/blocks/BlockCustomSpawner.java
@@ -9,10 +9,20 @@ import net.minecraft.block.SoundType;
 import net.minecraft.block.material.MapColor;
 import net.minecraft.block.material.Material;
 import net.minecraft.block.state.IBlockState;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.init.Blocks;
+import net.minecraft.item.ItemMonsterPlacer;
+import net.minecraft.item.ItemStack;
+import net.minecraft.tileentity.MobSpawnerBaseLogic;
+import net.minecraft.tileentity.TileEntity;
+import net.minecraft.tileentity.TileEntityMobSpawner;
 import net.minecraft.util.BlockRenderLayer;
+import net.minecraft.util.EnumActionResult;
 import net.minecraft.util.EnumFacing;
+import net.minecraft.util.EnumHand;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.IBlockAccess;
+import net.minecraft.world.World;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
 
@@ -31,5 +41,25 @@ public class BlockCustomSpawner extends BlockMobSpawner {
 	public BlockCustomSpawner setSoundType(SoundType sound) {
 		super.setSoundType(sound);
 		return this;
+	}
+
+	@Override
+	public boolean onBlockActivated(World worldIn, BlockPos pos, IBlockState state, EntityPlayer playerIn, EnumHand hand, EnumFacing facing, float hitX, float hitY, float hitZ) {
+		ItemStack itemstack = playerIn.getHeldItem(hand);
+		if (itemstack.getItem() instanceof ItemMonsterPlacer && playerIn.canPlayerEdit(pos.offset(facing), facing, itemstack)) {
+			IBlockState iblockstate = worldIn.getBlockState(pos);
+			TileEntity tileentity = worldIn.getTileEntity(pos);
+			if (tileentity instanceof TileEntityMobSpawner) {
+				MobSpawnerBaseLogic mobspawnerbaselogic = ((TileEntityMobSpawner)tileentity).getSpawnerBaseLogic();
+				mobspawnerbaselogic.setEntityId(ItemMonsterPlacer.getNamedIdFrom(itemstack));
+				tileentity.markDirty();
+				worldIn.notifyBlockUpdate(pos, iblockstate, iblockstate, 3);
+				if (!playerIn.capabilities.isCreativeMode) {
+					itemstack.shrink(1);
+				}
+				return true;
+			}
+		}
+		return false;
 	}
 }
