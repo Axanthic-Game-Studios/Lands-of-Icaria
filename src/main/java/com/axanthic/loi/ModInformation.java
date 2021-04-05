8e1d61cd261ab41ffa1ab1ec57046fa7b6b4dec7
RCXcrafter <rcxcrafter@gmail.com>
2021-04-05 20:15:39 +0200

    version: beta-1.12.2-1.0.1-hotfix

--- src/main/java/com/axanthic/loi/ModInformation.java
+++ src/main/java/com/axanthic/loi/ModInformation.java
@@ -5,7 +5,7 @@ public class ModInformation {
 	public static final String NAME = "Lands of Icaria";
 	public static final String ID = "landsoficaria";
 	public static final String DEPEND = "";
-	public static final String VERSION = "beta-1.12.2-1.0.1";
+	public static final String VERSION = "beta-1.12.2-1.0.1-hotfix";
 	public static final String CLIENTPROXY = "com.axanthic.loi.proxy.ClientProxy";
 	public static final String COMMONPROXY = "com.axanthic.loi.proxy.CommonProxy";
 	public static final String NETWORK_CHANNEL_NAME = "landsoficaria";
