0b6b6c956df851d7c9be47a391d2031390cc58ac
luxtracon <luxtracon@web.de>
2021-05-11 13:40:05 +0200

    fixed reed vines to return their corresponding block when picking it in creative mode

--- src/main/java/com/axanthic/loi/blocks/BlockBasicVineGrowingReeds.java
+++ src/main/java/com/axanthic/loi/blocks/BlockBasicVineGrowingReeds.java
@@ -7,6 +7,9 @@ import com.axanthic.loi.items.ItemResources;
 
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
 
 public class BlockBasicVineGrowingReeds extends BlockBasicVineGrowing {
 
@@ -31,5 +34,12 @@ public class BlockBasicVineGrowingReeds extends BlockBasicVineGrowing {
 	@Override
 	public int damageDropped(IBlockState state) {
 		return ItemResources.ResourceType.VINEREEDS.toMeta();
+	}
+
+	@Override
+	@SuppressWarnings("deprecation")
+	public ItemStack getItem(World worldIn, BlockPos pos, IBlockState state)
+	{
+		return new ItemStack(this);
 	}
 }
