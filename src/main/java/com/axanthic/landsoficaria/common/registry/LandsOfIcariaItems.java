3c1cc10a7edb5d7d768cb22a78fa5a372c9a99b3
luxtracon <luxtracon@web.de>
2022-02-01 23:21:45 +0100

    port of loam bricks

--- src/main/java/com/axanthic/landsoficaria/common/registry/LandsOfIcariaItems.java
+++ src/main/java/com/axanthic/landsoficaria/common/registry/LandsOfIcariaItems.java
@@ -15,6 +15,7 @@ public class LandsOfIcariaItems {
     public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, LandsOfIcariaInfo.MODID);
 
     public static final RegistryObject<Item> LOAM_LUMP = register("loam_lump", () -> new Item(new Item.Properties().tab(LandsOfIcariaTabs.creativeItems)));
+    public static final RegistryObject<Item> LOAM_BRICK = register("loam_brick", () -> new Item(new Item.Properties().tab(LandsOfIcariaTabs.creativeItems)));
     public static final RegistryObject<Item> CALCITE_POWDER = register("calcite_powder", () -> new Item(new Item.Properties().tab(LandsOfIcariaTabs.creativeItems)));
 
     private static <T extends Item> RegistryObject<T> register(final String name, final Supplier<T> item) {
