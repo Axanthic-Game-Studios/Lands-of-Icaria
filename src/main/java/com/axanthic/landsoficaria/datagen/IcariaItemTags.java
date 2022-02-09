58e85d7d7d9f663aae482c64cc35fdb567cca686
RCXcrafter <rcxcrafter@gmail.com>
2022-02-09 15:49:28 +0100

    add empty data generators

---
+++ src/main/java/com/axanthic/landsoficaria/datagen/IcariaItemTags.java
@@ -0,0 +1,29 @@
+package com.axanthic.landsoficaria.datagen;
+
+import com.axanthic.landsoficaria.LandsOfIcariaInfo;
+
+import net.minecraft.data.DataGenerator;
+import net.minecraft.data.tags.BlockTagsProvider;
+import net.minecraft.data.tags.ItemTagsProvider;
+import net.minecraft.tags.Tag.Named;
+import net.minecraft.world.item.Item;
+import net.minecraftforge.common.data.ExistingFileHelper;
+
+public class IcariaItemTags extends ItemTagsProvider {
+
+	public IcariaItemTags(DataGenerator gen, BlockTagsProvider blockTags, ExistingFileHelper existingFileHelper) {
+		super(gen, blockTags, LandsOfIcariaInfo.MODID, existingFileHelper);
+	}
+
+	@Override
+	protected void addTags() {
+
+	}
+
+	@SafeVarargs
+	public final void addAllTags(Item item, Named<Item>... tags) {
+		for (Named<Item> tag : tags) {
+			tag(tag).add(item);
+		}
+	}
+}
