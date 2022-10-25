093c3437ef33958ee526e34374f6d8a7ffdfd24c
luxtracon <luxtracon@web.de>
2022-10-25 18:53:22 +0200

    missing unstaged myrmeke fix

--- src/main/java/com/axanthic/icaria/common/entity/MyrmekeDroneEntity.java
+++ src/main/java/com/axanthic/icaria/common/entity/MyrmekeDroneEntity.java
@@ -42,7 +42,7 @@ import javax.annotation.ParametersAreNonnullByDefault;
 @ParametersAreNonnullByDefault
 
 public class MyrmekeDroneEntity extends Monster {
-    public EntityDataAccessor<Byte> CLIMBING = SynchedEntityData.defineId(MyrmekeDroneEntity.class, EntityDataSerializers.BYTE);
+    public static EntityDataAccessor<Byte> CLIMBING = SynchedEntityData.defineId(MyrmekeDroneEntity.class, EntityDataSerializers.BYTE);
 
     public MyrmekeDroneEntity(EntityType<? extends Monster> pType, Level pLevel) {
         super(pType, pLevel);
