c4b4deb80072cb90fd3e82d6a49ef2990f5b73ee
luxtracon <luxtracon@web.de>
2024-07-27 20:31:10 +0200

    fix missing animal health

--- src/main/java/com/axanthic/icaria/common/entity/SowEntity.java
+++ src/main/java/com/axanthic/icaria/common/entity/SowEntity.java
@@ -85,6 +85,7 @@ public class SowEntity extends IcariaAnimalEntity {
 		this.getAttribute(Attributes.ATTACK_DAMAGE).setBaseValue(size);
 		this.getAttribute(Attributes.ATTACK_KNOCKBACK).setBaseValue(size * 0.5D);
 		this.getAttribute(Attributes.MAX_HEALTH).setBaseValue(size * size);
+		this.setHealth(size * size);
 	}
 
 	public static AttributeSupplier.Builder registerAttributes() {
