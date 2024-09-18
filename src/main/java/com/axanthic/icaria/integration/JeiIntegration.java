7c38ca427f146d1f6d7b6a51bf2c608c753174fa
luxtracon <luxtracon@web.de>
2024-09-18 14:21:12 +0200

    add missing deprecation annotation

--- src/main/java/com/axanthic/icaria/integration/JeiIntegration.java
+++ src/main/java/com/axanthic/icaria/integration/JeiIntegration.java
@@ -32,7 +32,7 @@ import java.util.List;
 
 import javax.annotation.ParametersAreNonnullByDefault;
 
-@SuppressWarnings("unused")
+@SuppressWarnings("deprecation, unused")
 
 @MethodsReturnNonnullByDefault
 @ParametersAreNonnullByDefault
