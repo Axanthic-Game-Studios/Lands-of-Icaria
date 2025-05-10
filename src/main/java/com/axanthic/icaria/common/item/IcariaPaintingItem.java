0c45e6cffa2f12f5d95350fa2ef2f074d22a2be3
luxtracon <luxtracon@web.de>
2025-05-10 12:49:12 +0200

    fix Painting placement crash

--- src/main/java/com/axanthic/icaria/common/item/IcariaPaintingItem.java
+++ src/main/java/com/axanthic/icaria/common/item/IcariaPaintingItem.java
@@ -6,9 +6,14 @@ import com.axanthic.icaria.common.registry.IcariaEntityTypes;
 import javax.annotation.ParametersAreNonnullByDefault;
 
 import net.minecraft.MethodsReturnNonnullByDefault;
+import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
 import net.minecraft.world.InteractionResult;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.HangingEntityItem;
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.context.UseOnContext;
+import net.minecraft.world.level.Level;
 import net.minecraft.world.level.gameevent.GameEvent;
 
 @MethodsReturnNonnullByDefault
@@ -26,11 +31,19 @@ public class IcariaPaintingItem extends HangingEntityItem {
 		var level = pUseOnContext.getLevel();
 		var player = pUseOnContext.getPlayer();
 		var blockPos = pUseOnContext.getClickedPos().relative(direction);
-		var optional = IcariaPaintingEntity.create(blockPos, direction, level);
-		if (player != null && this.mayPlace(player, direction, itemStack, blockPos) && optional.isPresent() && optional.get().survives() && !level.isClientSide()) {
-			itemStack.consume(1, player);
-			level.addFreshEntity(optional.get());
-			level.gameEvent(player, GameEvent.ENTITY_PLACE, optional.get().position());
+		if (player != null && this.mayPlace(player, direction, itemStack, blockPos)) {
+			return this.handleAction(blockPos, direction, itemStack, level, player);
+		} else {
+			return InteractionResult.CONSUME;
+		}
+	}
+
+	public InteractionResult handleAction(BlockPos pBlockPos, Direction pDirection, ItemStack pItemStack, Level pLevel, Player pPlayer) {
+		var optional = IcariaPaintingEntity.create(pBlockPos, pDirection, pLevel);
+		if (optional.isPresent() && optional.get().survives() && !pLevel.isClientSide()) {
+			pItemStack.consume(1, pPlayer);
+			pLevel.addFreshEntity(optional.get());
+			pLevel.gameEvent(pPlayer, GameEvent.ENTITY_PLACE, optional.get().position());
 			optional.get().playPlacementSound();
 			return InteractionResult.SUCCESS;
 		} else {
