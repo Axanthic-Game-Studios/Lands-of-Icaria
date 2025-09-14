2f56f7a36700417428ac9c22ff4e8d13de8b1040
luxtracon <luxtracon@web.de>
2025-09-14 12:07:16 +0200

    refactor IcariaRegistrySetBuilder Entry Order

--- src/main/java/com/axanthic/icaria/data/IcariaRegistrySetBuilder.java
+++ src/main/java/com/axanthic/icaria/data/IcariaRegistrySetBuilder.java
@@ -15,6 +15,22 @@ import net.neoforged.neoforge.registries.NeoForgeRegistries;
 
 public class IcariaRegistrySetBuilder extends RegistrySetBuilder {
 	public IcariaRegistrySetBuilder() {
-		this.add(NeoForgeRegistries.Keys.BIOME_MODIFIERS, IcariaBiomeModifiers::bootstrap).add(Registries.BIOME, IcariaBiomes::bootstrap).add(Registries.CONFIGURED_FEATURE, IcariaConfiguredFeatures::bootstrap).add(Registries.DIMENSION_TYPE, IcariaDimensionTypes::bootstrap).add(IcariaRegistries.FEESH_VARIANT, IcariaFeeshVariants::bootstrap).add(IcariaRegistries.FICHE_VARIANT, IcariaFicheVariants::bootstrap).add(IcariaRegistries.FISSHH_VARIANT, IcariaFisshhVariants::bootstrap).add(IcariaRegistries.FYSH_VARIANT, IcariaFyshVariants::bootstrap).add(Registries.INSTRUMENT, IcariaInstruments::bootstrap).add(Registries.LEVEL_STEM, IcariaLevelStems::bootstrap).add(Registries.NOISE_SETTINGS, IcariaNoiseSettings::bootstrap).add(Registries.PAINTING_VARIANT, IcariaPaintingVariants::bootstrap).add(Registries.PLACED_FEATURE, IcariaPlacedFeatures::bootstrap).add(Registries.PROCESSOR_LIST, IcariaProcessorLists::bootstrap).add(Registries.STRUCTURE, IcariaStructures::bootstrap).add(Registries.STRUCTURE_SET, IcariaStructureSets::bootstrap).add(Registries.TEMPLATE_POOL, IcariaTemplatePools::bootstrap);
+		this.add(Registries.BIOME, IcariaBiomes::bootstrap)
+			.add(NeoForgeRegistries.Keys.BIOME_MODIFIERS, IcariaBiomeModifiers::bootstrap)
+			.add(Registries.CONFIGURED_FEATURE, IcariaConfiguredFeatures::bootstrap)
+			.add(Registries.DIMENSION_TYPE, IcariaDimensionTypes::bootstrap)
+			.add(IcariaRegistries.FEESH_VARIANT, IcariaFeeshVariants::bootstrap)
+			.add(IcariaRegistries.FICHE_VARIANT, IcariaFicheVariants::bootstrap)
+			.add(IcariaRegistries.FISSHH_VARIANT, IcariaFisshhVariants::bootstrap)
+			.add(IcariaRegistries.FYSH_VARIANT, IcariaFyshVariants::bootstrap)
+			.add(Registries.INSTRUMENT, IcariaInstruments::bootstrap)
+			.add(Registries.LEVEL_STEM, IcariaLevelStems::bootstrap)
+			.add(Registries.NOISE_SETTINGS, IcariaNoiseSettings::bootstrap)
+			.add(Registries.PAINTING_VARIANT, IcariaPaintingVariants::bootstrap)
+			.add(Registries.PLACED_FEATURE, IcariaPlacedFeatures::bootstrap)
+			.add(Registries.PROCESSOR_LIST, IcariaProcessorLists::bootstrap)
+			.add(Registries.STRUCTURE, IcariaStructures::bootstrap)
+			.add(Registries.STRUCTURE_SET, IcariaStructureSets::bootstrap)
+			.add(Registries.TEMPLATE_POOL, IcariaTemplatePools::bootstrap);
 	}
 }
