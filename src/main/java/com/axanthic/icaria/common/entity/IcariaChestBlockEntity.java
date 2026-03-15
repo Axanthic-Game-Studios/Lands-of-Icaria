6118eaaf29405f7528ca95d4d00d7e32cb66694b
luxtracon <luxtracon@web.de>
2026-03-15 16:00:56 +0100

    add Save Helper Method to IcariaChestBlockEntity

--- src/main/java/com/axanthic/icaria/common/entity/IcariaChestBlockEntity.java
+++ src/main/java/com/axanthic/icaria/common/entity/IcariaChestBlockEntity.java
@@ -43,13 +43,13 @@ public class IcariaChestBlockEntity extends ChestBlockEntity {
 	public int colorSouth;
 	public int colorWest;
 
+	public NonNullList<ItemStack> items = NonNullList.withSize(18, ItemStack.EMPTY);
+
 	public String labelUp;
 	public String labelNorth;
 	public String labelEast;
 	public String labelSouth;
 	public String labelWest;
-
-	public NonNullList<ItemStack> items = NonNullList.withSize(18, ItemStack.EMPTY);
 
 	public IcariaChestBlockEntity(BlockEntityType<?> pBlockEntityType, BlockPos pBlockPos, BlockState pBlockState) {
 		super(pBlockEntityType, pBlockPos, pBlockState);
@@ -57,6 +57,70 @@ public class IcariaChestBlockEntity extends ChestBlockEntity {
 
 	public IcariaChestBlockEntity(BlockPos pBlockPos, BlockState pBlockState) {
 		this(IcariaBlockEntityTypes.CHEST.get(), pBlockPos, pBlockState);
+	}
+
+	@Override
+	public int getContainerSize() {
+		return 18;
+	}
+
+	@Override
+	public void loadAdditional(ValueInput pValueInput) {
+		super.loadAdditional(pValueInput);
+		this.styleUp = pValueInput.getBooleanOr("StyleUp", false);
+		this.styleNorth = pValueInput.getBooleanOr("StyleNorth", false);
+		this.styleEast = pValueInput.getBooleanOr("StyleEast", false);
+		this.styleSouth = pValueInput.getBooleanOr("StyleSouth", false);
+		this.styleWest = pValueInput.getBooleanOr("StyleWest", false);
+		this.waxed = pValueInput.getBooleanOr("Waxed", false);
+		this.colorUp = pValueInput.getIntOr("ColorUp", 0);
+		this.colorNorth = pValueInput.getIntOr("ColorNorth", 0);
+		this.colorEast = pValueInput.getIntOr("ColorEast", 0);
+		this.colorSouth = pValueInput.getIntOr("ColorSouth", 0);
+		this.colorWest = pValueInput.getIntOr("ColorWest", 0);
+		this.labelUp = pValueInput.getStringOr("LabelUp", "");
+		this.labelNorth = pValueInput.getStringOr("LabelNorth", "");
+		this.labelEast = pValueInput.getStringOr("LabelEast", "");
+		this.labelSouth = pValueInput.getStringOr("LabelSouth", "");
+		this.labelWest = pValueInput.getStringOr("LabelWest", "");
+		if (!this.tryLoadLootTable(pValueInput)) {
+			ContainerHelper.loadAllItems(pValueInput, this.items);
+		}
+	}
+
+	@Override
+	public void saveAdditional(ValueOutput pValueOutput) {
+		super.saveAdditional(pValueOutput);
+		pValueOutput.putBoolean("StyleUp", this.styleUp);
+		pValueOutput.putBoolean("StyleNorth", this.styleNorth);
+		pValueOutput.putBoolean("StyleEast", this.styleEast);
+		pValueOutput.putBoolean("StyleSouth", this.styleSouth);
+		pValueOutput.putBoolean("StyleWest", this.styleWest);
+		pValueOutput.putBoolean("Waxed", this.waxed);
+		pValueOutput.putInt("ColorUp", this.colorUp);
+		pValueOutput.putInt("ColorNorth", this.colorNorth);
+		pValueOutput.putInt("ColorEast", this.colorEast);
+		pValueOutput.putInt("ColorSouth", this.colorSouth);
+		pValueOutput.putInt("ColorWest", this.colorWest);
+		this.saveString(pValueOutput, "LabelUp", this.labelUp);
+		this.saveString(pValueOutput, "LabelNorth", this.labelNorth);
+		this.saveString(pValueOutput, "LabelEast", this.labelEast);
+		this.saveString(pValueOutput, "LabelSouth", this.labelSouth);
+		this.saveString(pValueOutput, "LabelWest", this.labelWest);
+		if (!this.trySaveLootTable(pValueOutput)) {
+			ContainerHelper.saveAllItems(pValueOutput, this.items);
+		}
+	}
+
+	public void saveString(ValueOutput pValueOutput, String pName, @Nullable String pString) {
+		if (pString != null) {
+			pValueOutput.putString(pName, pString);
+		}
+	}
+
+	@Override
+	public void setItems(NonNullList<ItemStack> pItemStacks) {
+		this.items = pItemStacks;
 	}
 
 	public boolean getStyleUp() {
@@ -103,81 +167,29 @@ public class IcariaChestBlockEntity extends ChestBlockEntity {
 		return this.colorWest;
 	}
 
-	@Override
-	public int getContainerSize() {
-		return 18;
+	@Nullable
+	public String getLabelUp() {
+		return this.labelUp;
 	}
 
-	@Override
-	public void loadAdditional(ValueInput pValueInput) {
-		super.loadAdditional(pValueInput);
-
-		this.styleUp = pValueInput.getBooleanOr("StyleUp", false);
-		this.styleNorth = pValueInput.getBooleanOr("StyleNorth", false);
-		this.styleEast = pValueInput.getBooleanOr("StyleEast", false);
-		this.styleSouth = pValueInput.getBooleanOr("StyleSouth", false);
-		this.styleWest = pValueInput.getBooleanOr("StyleWest", false);
-
-		this.waxed = pValueInput.getBooleanOr("Waxed", false);
-
-		this.colorUp = pValueInput.getIntOr("ColorUp", 0);
-		this.colorNorth = pValueInput.getIntOr("ColorNorth", 0);
-		this.colorEast = pValueInput.getIntOr("ColorEast", 0);
-		this.colorSouth = pValueInput.getIntOr("ColorSouth", 0);
-		this.colorWest = pValueInput.getIntOr("ColorWest", 0);
-
-		this.labelUp = pValueInput.getStringOr("LabelUp", "");
-		this.labelNorth = pValueInput.getStringOr("LabelNorth", "");
-		this.labelEast = pValueInput.getStringOr("LabelEast", "");
-		this.labelSouth = pValueInput.getStringOr("LabelSouth", "");
-		this.labelWest = pValueInput.getStringOr("LabelWest", "");
-
-		if (!this.tryLoadLootTable(pValueInput)) {
-			ContainerHelper.loadAllItems(pValueInput, this.items);
-		}
+	@Nullable
+	public String getLabelNorth() {
+		return this.labelNorth;
 	}
 
-	@Override
-	public void saveAdditional(ValueOutput pValueOutput) {
-		super.saveAdditional(pValueOutput);
+	@Nullable
+	public String getLabelEast() {
+		return this.labelEast;
+	}
 
-		pValueOutput.putBoolean("StyleUp", this.styleUp);
-		pValueOutput.putBoolean("StyleNorth", this.styleNorth);
-		pValueOutput.putBoolean("StyleEast", this.styleEast);
-		pValueOutput.putBoolean("StyleSouth", this.styleSouth);
-		pValueOutput.putBoolean("StyleWest", this.styleWest);
+	@Nullable
+	public String getLabelSouth() {
+		return this.labelSouth;
+	}
 
-		pValueOutput.putBoolean("Waxed", this.waxed);
-
-		pValueOutput.putInt("ColorUp", this.colorUp);
-		pValueOutput.putInt("ColorNorth", this.colorNorth);
-		pValueOutput.putInt("ColorEast", this.colorEast);
-		pValueOutput.putInt("ColorSouth", this.colorSouth);
-		pValueOutput.putInt("ColorWest", this.colorWest);
-
-		if (this.labelUp != null) {
-			pValueOutput.putString("LabelUp", this.labelUp);
-		}
-
-		if (this.labelNorth != null) {
-			pValueOutput.putString("LabelNorth", this.labelNorth);
-		}
-
-		if (this.labelEast != null) {
-			pValueOutput.putString("LabelEast", this.labelEast);
-		}
-
-		if (this.labelSouth != null) {
-			pValueOutput.putString("LabelSouth", this.labelSouth);
-		}
-
-		if (this.labelWest != null) {
-			pValueOutput.putString("LabelWest", this.labelWest);
-		}
-
-		if (!this.trySaveLootTable(pValueOutput)) {
-			ContainerHelper.saveAllItems(pValueOutput, this.items);
-		}
+	@Nullable
+	public String getLabelWest() {
+		return this.labelWest;
 	}
 
 	public void setStyleUp(boolean pBold) {
@@ -245,11 +257,6 @@ public class IcariaChestBlockEntity extends ChestBlockEntity {
 	}
 
 	@Override
-	public void setItems(NonNullList<ItemStack> pItemStacks) {
-		this.items = pItemStacks;
-	}
-
-	@Override
 	public AbstractContainerMenu createMenu(int pId, Inventory pInventory) {
 		return new ChestMenu(MenuType.GENERIC_9x2, pId, pInventory, this, 2);
 	}
@@ -267,30 +274,5 @@ public class IcariaChestBlockEntity extends ChestBlockEntity {
 	@Override
 	public Packet<ClientGamePacketListener> getUpdatePacket() {
 		return ClientboundBlockEntityDataPacket.create(this);
-	}
-
-	@Nullable
-	public String getLabelUp() {
-		return this.labelUp;
-	}
-
-	@Nullable
-	public String getLabelNorth() {
-		return this.labelNorth;
-	}
-
-	@Nullable
-	public String getLabelEast() {
-		return this.labelEast;
-	}
-
-	@Nullable
-	public String getLabelSouth() {
-		return this.labelSouth;
-	}
-
-	@Nullable
-	public String getLabelWest() {
-		return this.labelWest;
 	}
 }
